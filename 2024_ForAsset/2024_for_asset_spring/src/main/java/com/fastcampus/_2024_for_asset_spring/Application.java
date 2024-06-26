cc8abc07a9ef277a19746048ec8f626e5f971009
genmu5 <genmu4@skuniv.ac.kr>
2024-06-26 22:50:40 +0900

    init

---
+++ 2024_ForAsset/2024_for_asset_spring/src/main/java/com/fastcampus/_2024_for_asset_spring/Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus._2024_for_asset_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 2024_ForAsset/2024_for_asset_spring/src/test/java/com/fastcampus/_2024_for_asset_spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus._2024_for_asset_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
